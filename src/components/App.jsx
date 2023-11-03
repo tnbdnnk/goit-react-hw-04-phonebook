f5cb8c597510c41e8f8a0db5ae3f79ae571c79fb
Tatiana Bidnenko <tnbdnnk@gmail.com>
2023-11-03 21:59:04 +0200

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,85 +1,70 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 
 import css from './App.module.css';
 
-export class App extends Component {
-    state = {
-        contacts: [],
-        filter: '',
+export const App = () => {
+    const [contacts, setContacts] = useState(
+        JSON.parse(localStorage.getItem('contacts')) || []
+    );
+
+    const [filter, setFilter] = useState('');
+
+    useEffect(() => {
+        const localStorageData = JSON.parse(localStorage.getItem('contacts'));
+        setContacts(() => [...localStorageData]);
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('contacts', JSON.stringify(contacts));
+    }, [contacts]);
+
+    const handleFilterChange = e => {
+        setFilter(() => e.target.value);
     };
 
-    handleInputChange = e => {
-        this.setState({ [e.target.name]: e.target.value });
-    };
-
-    addContact = (name, number, id) => {
-        if (this.state.contacts.find(
+    const addContact = (name, number, id) => {
+        if (contacts.find(
                 contact => contact.name.toLowerCase() === name.toLowerCase())
                 ) {
                     alert(`${name} is already in contacts.`);
                     return;
                 }
         
-        this.setState(prevState => {
-            return {
-                contacts: [...prevState.contacts, { name, number, id }],
-            };
-        });
+        setContacts(prevState => [...prevState, { name, number, id}]);
     };
 
-    filterContacts = () => {
-        return this.state.contacts.filter(contact => {
+    const filterContacts = () => {
+        return contacts.filter(contact => {
             return contact.name
                 .toLowerCase()
-                .includes(this.state.filter.toLowerCase());
+                .includes(filter.toLowerCase());
         });
     };
 
-    deleteContact = id => {
-        const contacts = this.state.contacts.filter(contact => {
+    const deleteContact = id => {
+        const deleteContacts = contacts.filter(contact => {
             return contact.id.toLowerCase() !== id.toLowerCase();
         });
 
-        this.setState(() => {
-            return {
-                contacts: [...contacts],
-            };
-        });
+        setContacts(() => [...deleteContacts]);
     };
 
-    componentDidMount() {
-        const contacts = localStorage.getItem('contacts');
-        const parsedContacts = JSON.parse(contacts);
-    
-        if (parsedContacts) {
-            this.setState({ contacts: parsedContacts });
-        }
-    }
-    
-    componentDidUpdate(prevProps, prevState) {
-        if (this.state.contacts !== prevState.contacts) {
-            localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-        }
-    }
-
-    render() {
-        return (
-            <div className={css.container}>
-                <h2 className={css.title}>Phonebook</h2>
-                <ContactForm addContact={this.addContact}/>
-                <h2 className={css.title}>Contacts</h2>
-                <Filter
-                    filter={this.state.filter}
-                    handleFilterChange={this.handleInputChange}
-                />
-                <ContactList
-                    contacts={this.filterContacts()}
-                    deleteContact={this.deleteContact}
-                />
-            </div>
-        )
-    }
+    return (
+        <div className={css.container}>
+            <h2 className={css.title}>Phonebook</h2>
+            <ContactForm addContact={addContact}/>
+            <h2 className={css.title}>Contacts</h2>
+            <Filter
+                filter={filter}
+                handleFilterChange={handleFilterChange}
+            />
+            <ContactList
+                contacts={filterContacts()}
+                deleteContact={deleteContact}
+            />
+        </div>
+    )
 }
