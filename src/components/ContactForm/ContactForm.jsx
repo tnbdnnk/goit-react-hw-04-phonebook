fa8f2c0a6da08ac233d8a2234ee97ec0bcd52591
Tatiana Bidnenko <tnbdnnk@gmail.com>
2023-12-18 11:46:20 +0200

    fix patterns

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -37,7 +37,7 @@ export class ContactForm extends Component {
                         type='text'
                         name='name'
                         value={this.state.name}
-                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я])?[a-zA-Zа-яА-Я]*)*$"
+                        pattern="^[a-zA-Zа-яА-Я]+([ -'][a-zA-Zа-яА-Я]+)*$"
                         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                         required
                         onChange={this.handleInputChange} 
@@ -51,7 +51,7 @@ export class ContactForm extends Component {
                         type='tel'
                         name='number'
                         value={this.state.number}
-                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                        pattern="^[0-9]*$"
                         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                         required
                         onChange={this.handleInputChange}
